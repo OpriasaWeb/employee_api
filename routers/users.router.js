fff5f9d2b1c6c8515d5673c35fea82e3a1f5f7ee
Jeremy Opriasa <opriasajbusiness@gmail.com>
2024-04-10 18:06:16 +0800

    fix: add admin count router on users controller

--- routers/users.router.js
+++ routers/users.router.js
@@ -6,5 +6,6 @@ const UsersController = require("../controllers/users.controller")
 router.post("/register", UsersController.register)
 router.get("/hash", UsersController.hash)
 router.post("/login", UsersController.login)
+router.get("/adminCount", UsersController.userCount)
 
 module.exports = router
